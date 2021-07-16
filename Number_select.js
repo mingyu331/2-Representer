a6ced0163b4808d30b2e3b0e1d11ed5fe35e6c91
Mingyu <mingyu331@naver.com>
2021-07-16 22:22:24 +0900

    v
    b

--- Number_select.js
+++ Number_select.js
@@ -1,7 +1,11 @@
-const num = 22
-const min = 1000
-const max = 4900
+const num = 22;
+const min = 1000;
+const max = 4900;
+const fs = require('fs');
 
+let numbers = [];
 for (let i = 0; i < num; i++) {
-    console.log(min + Math.floor((max - min) * Math.random()));
+    numbers.push(min + Math.floor((max - min) * Math.random()));
 }
+
+fs.writeFileSync(".\\representer\\numbers.txt", JSON.stringify(numbers));
